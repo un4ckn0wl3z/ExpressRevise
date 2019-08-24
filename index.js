06baa801f6855fa9084a9f907379adf7f682a03e
un4ckn0wl3z <slickerth@gmail.com>
2019-08-24 21:33:24 +0700

    completed

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 const names =
     [
-        { id: 1, names: 'Anuwat' },
-        { id: 2, names: 'Pansa' },
-        { id: 3, names: 'Surachad' }
+        { id: 1, name: 'Anuwat' },
+        { id: 2, name: 'Pansa' },
+        { id: 3, name: 'Surachad' }
     ];
 
 app.get('/', (req, res) => {
@@ -18,8 +20,40 @@ app.get('/api/names', (req, res) => {
 
 app.get('/api/names/:id', (req, res) => {
     const name = names.find(m => m.id === parseInt(req.params.id));
-    if (!name) res.status(404).send({ message: 'Data not found.' });
+    if (!name) return res.status(404).send({ message: 'Data not found.' });
     res.send(name);
+});
+
+app.post('/api/names', (req, res) => {
+
+    const result = validateName(req.body);
+
+    if (result.error) {
+        res.status(400).send({ message: result.error.details[0].message });
+        return;
+    }
+    const name = {
+        id: names.length + 1,
+        name: req.body.name
+    }
+    names.push(name);
+    res.status(201).send(name);
+});
+
+app.put('/api/names/:id', (req, res) => {
+
+    const name = names.find(m => m.id === parseInt(req.params.id));
+    if (!name) return res.status(404).send({ message: 'Data not found.' });
+
+    const result = validateName(req.body);
+    if (result.error) {
+        res.status(400).send({ message: result.error.details[0].message });
+        return;
+    }
+
+    name.name = req.body.name;
+    res.status(200).send(name);
+
 });
 
 app.get('/api/posts/:year/:month', (req, res) => {
@@ -30,6 +64,23 @@ app.get('/api/posts/:year/:month', (req, res) => {
     }
     res.send(postsParams);
 });
+
+app.delete('/api/names/:id', (req, res) => {
+    const name = names.find(m => m.id === parseInt(req.params.id));
+    if (!name) return res.status(404).send({ message: 'Data not found.' });
+    const nameIndex = names.indexOf(name);
+    names.splice(nameIndex, 1);
+    res.send(name);
+});
+
+function validateName(name) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+
+    return Joi.validate(name, schema);
+
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
